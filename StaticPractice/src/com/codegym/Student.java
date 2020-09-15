66a6c19c972526b227574c2970e4d1dac1389c44
viet <vietdq2412@gmail.com>
2020-09-15 14:06:01 +0700

    add stactic practice

---
+++ StaticPractice/src/com/codegym/Student.java
@@ -0,0 +1,20 @@
+package com.codegym;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    public Student(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    static void change(){
+        college = "CodeGym";
+    }
+
+    void display(){
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
