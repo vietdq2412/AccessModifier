77627b1c3af8f3f2e9da94e5792b93bc29e80676
viet <vietdq2412@gmail.com>
2020-09-15 14:19:46 +0700

    StaticPropertyPractice

---
+++ StaticPropertyPractice/src/car/Car.java
@@ -0,0 +1,26 @@
+package car;
+
+public class Car {
+    private String name;
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
+}
