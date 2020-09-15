03c016c831d31932e7ae41bddaa69357c0e64304
viet <vietdq2412@gmail.com>
2020-09-15 14:26:06 +0700

    add AccessModifierEX

---
+++ AccessModifierEX/src/Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+    private double radius = 1;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ AccessModifierEX/src/run.java
@@ -0,0 +1,8 @@
+public class run {
+    public static void main(String[] args) {
+        Circle circle = new Circle(4, "blue");
+
+        System.out.println("color: "+ circle.getColor());
+        System.out.println("radius: "+ circle.getRadius());
+    }
+}
